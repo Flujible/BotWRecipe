815e4b158c9635c93a6ee1ad1635718c06cb21c1
George Bryant <george.s.bryant@gmail.com>
2018-09-13 22:15:29 +0100

    Create object literal for each attribute icon and give bespoke styling based on value

--- src/components/Attribute/Attribute.style.js
+++ src/components/Attribute/Attribute.style.js
@@ -4,9 +4,38 @@ const StyledAttribute = styled.p`
 
 `;
 
+const getType = (type) => {
+  return {
+    //Health
+    '': '#ec2434',
+    //Electro
+    '': '#fcfb0a',
+    //Hasty
+    '': '#108fff',
+    //Spicy - the second character needs to lay over the first and have this color: #fbfbe1
+    '': '#a3fcfe',
+    //Chilly
+
+    //Mighty
+
+    //Hearty
+
+    //Energizing
+
+    //Enduring
+
+    //Tough
+
+    //Sneaky
+
+    //Fireproof
+
+  }[type];
+};
+
 const StyledFont = styled.span`
   font-family: botw-icons-dual-color;
-  color: #ec2434;
+  color: ${props => getType(props.children.charAt(0))};
 `;
 
 export {StyledAttribute, StyledFont};
