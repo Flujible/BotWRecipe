3a1c4051982d29e1d0b939456dabfc4093633188
George Bryant <george.s.bryant@gmail.com>
2018-09-13 17:11:14 +0100

    Remove unnecessary logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import StyledHeader from './Header.style.js';
+import styled from 'styled-components';
 
-const Header = ({children}) => {
-  return <StyledHeader>{children}</StyledHeader>;
-}
+const Header = styled.h2`
+  font-family: TheWildBreathOfZelda, Arial, sans-serif;
+  font-size: 3em;
+  border-bottom: solid;
+  border-width: 1px;
+  padding-bottom: 15px;
+`;
 
 Header.description = `
   Headers are used to title a recipe with its given name from the game.
@@ -13,5 +17,7 @@ Header.propTypes = {
   /** Text that appears in the title*/
   children: PropTypes.string.isRequired,
 }
+
+Header.displayName = "Header"
 
 export default Header;
