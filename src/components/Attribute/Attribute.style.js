bd789afa9ec93a5653ecc318af761470a98b22f4
George Bryant <george.s.bryant@gmail.com>
2018-09-16 16:42:02 +0100

    Import new lib

--- src/components/Attribute/Attribute.style.js
+++ src/components/Attribute/Attribute.style.js
@@ -1,36 +1,16 @@
 import styled from 'styled-components';
+import icons from '../../libs/icons.js'
 
 const StyledAttribute = styled.p`
 
 `;
 
-const getType = (type) => {
-  return {
-    //Health
-    '': '#ec2434',
-    //Electro
-    '': '#fcfb0a',
-    //Hasty
-    '': '#108fff',
-    //Spicy
-    '': '#a3fcfe',
-    //Chilly
-    '': '#ff9902',
-    //Mighty
-    '': '#fbfbe1',
-    //Hearty
-    '': '#fcfb0a',
-    //Energizing
-    '': '#05e704',
-    //Enduring
-    '': '#fcfb0a',
-    //Tough
-    '': '#f3f3cf',
-    //Sneaky
-    '': '#d03bfe',
-    //Fireproof
-    '': '#ec444a',
-  }[type];
+const getColour = (title) => {
+  return icons[title].color;
+};
+
+const getSecColour = (title) => {
+  return icons[title].secondaryColor;
 };
 
 const StyledFont = styled.span`
@@ -39,4 +19,4 @@ const StyledFont = styled.span`
   color: ${props => getType(props.children.charAt(0))};
 `;
 
-export {StyledAttribute, StyledFont};
+export {StyledAttribute, StyledFont, StyledIconPart};
