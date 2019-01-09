1f5af6c5e5de15bdfe9f9ff17fa7cc6fcd2ee917
George Bryant <gbryant@emea.corp.jwt.com>
2019-01-09 19:21:57 +0000

    Alter to include image and container div

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Image from "../Image/Image";
 
-const Header = styled.h2`
-	font-family: TheWildBreathOfZelda, Arial, sans-serif;
-	font-size: 3em;
+const HeaderDiv = styled.div`
 	border-bottom: solid;
 	border-width: 1px;
 	padding-bottom: 15px;
 `;
+
+const StyledHeader = styled.h2`
+	font-family: TheWildBreathOfZelda, Arial, sans-serif;
+	font-size: 3em;
+	display: inline-block;
+	margin: 0 0 0 20px;
+`;
+
+const Header = ({ imgSrc, children }) => (
+	<HeaderDiv>
+		<Image src={imgSrc} />
+		<StyledHeader>{children}</StyledHeader>
+	</HeaderDiv>
+);
 
 Header.description = `
   Headers are used to title a recipe with its given name from the game.
