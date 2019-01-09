2144fa02500dd5c78b317fcfe7af9ca0d2b39b5b
George Bryant <gbryant@emea.corp.jwt.com>
2019-01-09 19:23:05 +0000

    Add more styles

--- src/components/RecipeCard/RecipeCard.style.js
+++ src/components/RecipeCard/RecipeCard.style.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
+import Img from "../Image/Image";
 
 const StyledRecipeCard = styled.div`
-	border-radius: 20%;
+	border: solid;
+	border-radius: 5%;
+	padding: 20px;
+	width: 50%;
+	margin: 0 auto;
+	display: block;		
+	background-color: white;
 `;
 
 export default StyledRecipeCard;
