61eb397ee14ff2b3dc1094f54ba67af53fd44c7a
George Bryant <gbryant@emea.corp.jwt.com>
2019-01-09 19:22:57 +0000

    Rename stories file

---
+++ src/components/RecipeCard/RecipeCard.stories.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { storiesOf, action, withInfo } from "../../stories";
+
+import RecipeCard from "./RecipeCard";
+
+const attributes = {
+    "Health": 2.5,
+    "Chilly": 3,
+};
+const buttonText = "Edit";
+const buttonOnClick = action("clicked");
+const headerText = "Pumpkin Stew";
+const imgSrc = "http://botw-recipes.com/assets/images/stuff/recipes/10.png";
+const ingredients = [
+	{
+		name: "Fresh milk",
+		quantity: 1,
+	},
+	{
+		name: "Tabantha wheat",
+		quantity: 1,
+	},
+	{
+		name: "Goat butter",
+		quantity: 1,
+	},
+	{
+		name: "Pumpkin",
+		quantity: 1,
+	},
+];
+
+
+storiesOf("RecipeCard", module)
+    .addDecorator((story, context) => 
+        withInfo(RecipeCard.description)(story)(context),
+    )
+    .add("Pimpkin Stew", () => <RecipeCard attributes={attributes} buttonOnClick={buttonOnClick} buttonText={buttonText} headerText={headerText} imgSrc={imgSrc} ingredients={ingredients} />);
