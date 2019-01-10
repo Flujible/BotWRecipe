7f4531ba4a4f615a7b19d7985c42b7ea640800a7
George Bryant <gbryant@emea.corp.jwt.com>
2019-01-10 10:05:00 +0000

    Align image with text

--- src/components/Image/Image.style.js
+++ src/components/Image/Image.style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const StyledImage = styled.img`
     display: inline-block;
     margin: 2px;
+    vertical-align: text-bottom;
 `;
 
 export default StyledImage;
