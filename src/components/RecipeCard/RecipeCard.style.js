a9a01326eb2e8c37fbb23d222b1bcdbba8eb8f00
George Bryant <george.s.bryant@gmail.com>
2019-01-17 20:47:11 +0000

    Remove restrictive width property

--- src/components/RecipeCard/RecipeCard.style.js
+++ src/components/RecipeCard/RecipeCard.style.js
@@ -5,7 +5,6 @@ const StyledRecipeCard = styled.div`
 	box-shadow: 0 2px 6px 0 hsla(0, 0%, 0%, 0.2);
 	border-radius: 8px;
 	padding: 20px;
-	width: 50%;
 	max-width: 800px;
 	margin: 0 auto 50px auto;
 	display: block;		
