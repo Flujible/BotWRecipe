0c748ebd8e5affcfd0a5b06e8b2fd4f2524f7401
George Bryant <george.s.bryant@gmail.com>
2019-01-17 20:47:26 +0000

    Replace sample data include with API call

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -2,22 +2,27 @@ import React from "react";
 import PropTypes from "prop-types";
 import StyledRecipes from "./Recipes.style";
 import RecipeCard from '../RecipeCard/RecipeCard';
-import recipeJSON from '../../recipes';
 
 class Recipes extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            recipes: recipeJSON.recipes,
-        };
+        this.state = {recipes: []};
     }
 
     componentDidMount() {
+        fetch("http://localhost:3001/api/recipes")
+            .then(res => {
+                return res.json()})
+            .then(result => {
+                this.setState(result)
+            },
+            (error) => {
+                console.log("oh shit, there was a problem")
+            });
     }
 
     generateCards(recipesArr) {
         return recipesArr.map(recipe => {
-            console.log(recipe.imgSrc);
             return <RecipeCard attributes={recipe.effects} headerText={recipe.name} imgSrc={recipe.img} ingredients={recipe.ingredients}/>
         })
     }
