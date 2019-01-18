bada154266e248bf158d713d335d681c90f62055
George Bryant <george.bryant@mirumagency.com>
2019-01-18 22:56:53 +0000

    Add bespoke data file

---
+++ recipeData.js
@@ -0,0 +1,103 @@
+module.exports = {
+	recipes: [
+		{
+			name: "Monster Cake",
+			ingredients: [
+				{
+					name: "Monster extract",
+					quantity: 1,
+				},
+				{
+					name: "Tabantha wheat",
+					quantity: 1,
+				},
+				{
+					name: "Cane sugar",
+					quantity: 1,
+				},
+				{
+					name: "Goat butter",
+					quantity: 1,
+				},
+			],
+			img: "http://botw-recipes.com/assets/images/stuff/recipes/5.png",
+			effects: [
+				{
+					name: "Health",
+					value: 2,
+				},
+			],
+		},
+		{
+			name: "Meat-stuffed pumpkin",
+			ingredients: [
+				{
+					name: "pumpkin",
+					quantity: 1,
+				},
+				{
+					name:
+						"Raw gourmet meat, Raw whole bird, Raw prime meat, Raw bird thigh, Raw Meat or Raw bird drumstick",
+					quantity: 1,
+				},
+			],
+			img: "http://botw-recipes.com/assets/images/stuff/recipes/65.png",
+			effects: [
+				{
+					name: "Tough",
+					value: 1,
+					duration: "1:20",
+				},
+			],
+		},
+		{
+			name: "Fruitcake",
+			ingredients: [
+				{
+					name: "Apple or Wildberry",
+					quantity: 1,
+				},
+				{
+					name:
+						"Hearty duran, palm fruit, hydromelonspicy pepper, voltfruit, fleet-lotus seeds or might banana",
+					quantity: 1,
+				},
+				{
+					name: "Tabantha wheat",
+					quantity: 1,
+				},
+				{
+					name: "Cane sugar",
+					quantity: 1,
+				},
+			],
+			img: "http://botw-recipes.com/assets/images/stuff/recipes/1.png",
+		},
+		{
+			name: "Seafood paella",
+			ingredients: [
+				{
+					name: "Mighty porgy or armoured porgy",
+					quantity: 1,
+				},
+				{
+					name: "Hearty blueshell snail",
+					quantity: 1,
+				},
+				{
+					name: "Hylian rice",
+					quantity: 1,
+				},
+				{
+					name: "Goat butter",
+					quantity: 1,
+				},
+				{
+					name: "Salt",
+					quantity: 1,
+				},
+			],
+			img: "http://botw-recipes.com/assets/images/stuff/recipes/2.png",
+		},
+	],
+};
