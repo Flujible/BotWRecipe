e7a896d6637786428f749a3b813ef255efefcc7a
George Bryant <george.bryant@mirumagency.com>
2019-01-18 22:59:03 +0000

    Add CSS to base app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Recipes from "../src/components/Recipes/Recipes";
+import "./base.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Recipes />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Recipes />
+			</div>
+		);
+	}
 }
 
 export default App;
