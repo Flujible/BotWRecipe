58120cb1ad10db76843901884149b68b0ec7a4e0
George Bryant <george.s.bryant@gmail.com>
2019-01-25 21:33:06 +0000

    Add header to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
-import Recipes from "../src/components/Recipes/Recipes";
-import "./base.css";
+import React, { Component } from 'react';
+import Recipes from '../src/components/Recipes/Recipes';
+import Header from './components/Header/Header';
+import './base.css';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Header>Chef of the wilD</Header>
 				<Recipes />
 			</div>
 		);
